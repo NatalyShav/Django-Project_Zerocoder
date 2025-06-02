c1d9a5e7bb05cd6fb8634bc4a94c7800153d1ef2
NatalyShav <shaverinanatalia@gmail.com>
2025-06-02 14:53:18 +0300

    add new files

--- movie_project/films/views.py
+++ movie_project/films/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Film
+from .forms import FilmForm
 
-# Create your views here.
+
+def add_film(request):
+    error = ''
+    if request.method == 'POST':
+        form = FilmForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_film')  # замените на нужный URL или имя маршрута
+        else:
+            error = 'Пожалуйста, исправьте ошибки в форме.'
+
+    form = FilmForm()
+    return render(request, 'films/add_film.html', {'form': form, 'error': error})
+
+
+def list_films(request):
+    films = Film.objects.all()
+    return render(request, 'films/list_films.html', {'films': films})
+
+
+
+
+
